780a86d3c72195b729a6d39461a76ea842b17ede
rgohds <127057257+rgohds@users.noreply.github.com>
2024-04-14 22:25:07 -0600

    travis CI and unit test
    travis CI and unit test

--- src/test/java/com/RE/springboot/backend/apirest/SpringBootBackendApirestRe1ApplicationTests.java
+++ src/test/java/com/RE/springboot/backend/apirest/SpringBootBackendApirestRe1ApplicationTests.java
@@ -1,13 +1,35 @@
 package com.RE.springboot.backend.apirest;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+  webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+  classes = SpringBootBackendApirestRe1Application.class)
+@AutoConfigureMockMvc
 class SpringBootBackendApirestRe1ApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@Test
-	void contextLoads() {
+	void contextLoads1() throws Exception {
+		mockMvc.perform(get("/api/instructores")).andExpect(status().isOk());
 	}
+
+	/*
+	@Test
+	void contextLoads2() throws Exception {
+		mockMvc.perform(get("/api/instructores2")).andExpect(status().isOk());
+	}*/
 
 }
